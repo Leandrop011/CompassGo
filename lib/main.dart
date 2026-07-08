4205de5830963c2ca867230ef7d06bcfc9e8822c
Leandro Pozo <jordanspozo@gmail.com>
2026-07-08 11:46:15 -0500

    Init the section 05 for latitude and longitude.

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:flutter/services.dart';
 import 'package:compass_app/config/config.dart';
 
 void main() async{
-  // !  SECCION 04 - MAP
+  // !  SECCION 05 - LAT AND LONG
   WidgetsFlutterBinding.ensureInitialized();
 
   // ? oculart la barra/botones de navegacion
